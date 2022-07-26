db68c67592ddff1984b65446e863900f0c04eb23
letswinoff <sumitsahoof5@gmail.com>
2022-07-26 22:21:20 +0530

    remove metaname

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -15,7 +15,7 @@ const Signup = (props) => {
       props.showAlert("Password and Confirm Password needs to match.", "danger");
       return;
     }
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("http://ancient-hamlet-43038.herokuapp.com/api/auth/createuser", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
